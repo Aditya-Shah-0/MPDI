195e400cc37ea928a95922a2ee4681767e0efd7b
Aditya-shah-0 <adityashah.chabua@gmail.com>
2025-11-07 16:46:12 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss(),react()],  
+  build: {
+    chunkSizeWarningLimit: 1000, // <-- Set this to a larger value (in kB)
+  },
+});
